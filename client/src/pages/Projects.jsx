52584116ac5d59179472c554b7e00d7ed4047e3c
Sanket Jagtap <sanketjagtap4622@gmail.com>
2024-08-31 20:01:56 +0530

    complete about and project pages

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,7 +1,16 @@
 const Projects = () => {
   return (
-    <div>Projects</div>
-  )
-}
+    <div className="min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3">
+      <h1 className="text-3xl font-semibold">Projects</h1>
+      <p className="text-md text-gray-500">Nothing to show yet. Coming Soon...</p>
+      <a
+        href="https://github.com/sanketjagtap4622"
+        className="text-md text-gray-500 hover:underline hover:text-blue-500"
+      >
+        GitHub
+      </a>
+    </div>
+  );
+};
 
-export default Projects
+export default Projects;
